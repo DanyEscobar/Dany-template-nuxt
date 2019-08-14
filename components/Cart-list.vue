d1d07483659e765da1ce795cfbc5411adfa2641e
Dany Escobar <daes_1995@hotmail.com>
2019-08-14 16:41:45 -0500

    Create the new component for render

---
+++ components/Cart-list.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <div class="contenedor">adsa
+      <div class="cart-item">ASDASD
+
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+.contenedor {
+  display: flex;
+  background-color: aquamarine;
+  width: 100%;
+  justify-content: space-around;
+}
+.cart-item {
+  background-color: brown;
+  flex: 1;
+}
+</style>
