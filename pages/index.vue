0924c7e570ad8bb560692dfd3074110d0854e460
Dany Escobar <daes_1995@hotmail.com>
2019-08-14 10:35:16 -0500

    Add new component for render

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
   <section>
     <cart></cart>
+    <categories></categories>
   </section>
 </template>
 
 <script>
 import Cart from '../components/Cart'
+import Categories from '../components/Categories'
 export default {
-  components: { Cart }
+  components: { Cart, Categories },
 }
 </script>
 
